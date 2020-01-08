4ad564994f45252069305eb389b977ec9285ee3a
Devi Prasad Ghimire <devi.ghimire@mport.com.au>
2020-01-08 12:32:44 +1100

    Build system working : DG

--- SRActivityIndicator.swift
+++ SRActivityIndicator.swift
@@ -11,7 +11,7 @@ import UIKit
 @IBDesignable
 public class SRActivityIndicator: UIView {
     
-    @IBInspectabledsfsdf dfdsf 
+    @IBInspectable
     public var hidesWhenStopped : Bool = true
     @IBInspectable
     public var outerFillColor : UIColor = UIColor.clear
