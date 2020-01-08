5cc40bf9d3ed297e7c5f6fd00dbba6ebcba44b02
Devi Prasad Ghimire <devi.ghimire@mport.com.au>
2020-01-08 11:25:49 +1100

    Code Refactor:

--- Example/SRActivityIndicator/ViewController.swift
+++ Example/SRActivityIndicator/ViewController.swift
@@ -9,53 +9,50 @@
 import UIKit
 
 class ViewController: UIViewController {
-
-  var activityIndicator =  SRActivityIndicator()
-           
-           override func viewDidLoad() {
-               super.viewDidLoad()
-               activityIndicator.innerStrokeColor = UIColor.white
-               activityIndicator.outerStrokeColor = UIColor.clear
-               activityIndicator.centerImage = UIImage(named: "globe.jpg")!
-               activityIndicator.show()
-        }
-        
-       
-        
-        @IBAction func showAlert(sender: AnyObject) {
-            let alert = UIAlertController(title: "Change Color", message: "Please Select an Option", preferredStyle: .actionSheet)
-
-            alert.addAction(UIAlertAction(title: "OuterStrokeColor", style: .default , handler:{ (UIAlertAction)in
-                self.activityIndicator.outerStrokeColor = UIColor.yellow
-                self.activityIndicator.draw(self.activityIndicator.frame)
-            }))
-
-            alert.addAction(UIAlertAction(title: "innerStrokeColor", style: .default , handler:{ (UIAlertAction)in
-                self.activityIndicator.innerStrokeColor = UIColor.green
-                self.activityIndicator.draw(self.activityIndicator.frame)
-            }))
-
-            alert.addAction(UIAlertAction(title: "outerFillColor", style: .default , handler:{ (UIAlertAction)in
-                self.activityIndicator.outerFillColor = UIColor.white
-                self.activityIndicator.draw(self.activityIndicator.frame)
-            }))
-
-            alert.addAction(UIAlertAction(title: "CenterImage", style: .default, handler:{ (UIAlertAction)in
-                self.activityIndicator.centerImage = UIImage(named: "image.jpg")!
-                self.activityIndicator.draw(self.activityIndicator.frame)
-
-            }))
-
-            self.present(alert, animated: true, completion: {
-                print("completion block")
-            })
-        }
-        
-        
-        @IBAction func stopAnimating(_ sender: Any) {
-            self.activityIndicator.dissmiss()
-        }
-        
-
+    
+    var activityIndicator =  SRActivityIndicator()
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        activityIndicator.innerStrokeColor = UIColor.white
+        activityIndicator.outerStrokeColor = UIColor.clear
+        activityIndicator.centerImage = UIImage(named: "globe.jpg")!
+        activityIndicator.show().hidesWhenStopped = true
     }
+        
+    @IBAction func showAlert(sender: AnyObject) {
+    
+        let alert = UIAlertController(title: "Change Color", message: "Please Select an Option", preferredStyle: .actionSheet)
+        
+        alert.addAction(UIAlertAction(title: "OuterStrokeColor", style: .default , handler:{ (UIAlertAction)in
+            self.activityIndicator.outerStrokeColor = UIColor.yellow
+            self.activityIndicator.draw(self.activityIndicator.frame)
+        }))
+        
+        alert.addAction(UIAlertAction(title: "innerStrokeColor", style: .default , handler:{ (UIAlertAction)in
+            self.activityIndicator.innerStrokeColor = UIColor.green
+            self.activityIndicator.draw(self.activityIndicator.frame)
+        }))
+        
+        alert.addAction(UIAlertAction(title: "outerFillColor", style: .default , handler:{ (UIAlertAction)in
+            self.activityIndicator.outerFillColor = UIColor.white
+            self.activityIndicator.draw(self.activityIndicator.frame)
+        }))
+        
+        alert.addAction(UIAlertAction(title: "CenterImage", style: .default, handler:{ (UIAlertAction)in
+            self.activityIndicator.centerImage = UIImage(named: "image.jpg")!
+            self.activityIndicator.draw(self.activityIndicator.frame)
+            
+        }))
+        
+        self.present(alert, animated: true, completion: {
+            print("completion block")
+        })
+    }
+    
+    
+    @IBAction func stopAnimating(_ sender: Any) {
+        self.activityIndicator.dissmiss()
+    }
+}
 
